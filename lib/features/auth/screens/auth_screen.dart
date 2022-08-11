9233126e01428d2259a07e4ced1e9dc52dad5bdf
Nikita Mishkov <mishkov.dd@gmail.com>
2022-08-11 19:01:15 +0300

    Implement login

--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:surf_practice_chat_flutter/features/auth/exceptions/auth_exception.dart';
 import 'package:surf_practice_chat_flutter/features/auth/models/token_dto.dart';
 import 'package:surf_practice_chat_flutter/features/auth/repository/auth_repository.dart';
 import 'package:surf_practice_chat_flutter/features/chat/repository/chat_repository.dart';
@@ -23,10 +27,78 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  // TODO(task): Implement Auth screen.
+  final _loginController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _isSignInInProcess = false;
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: _loginController,
+              decoration: InputDecoration(
+                label: const Text('Login'),
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 8),
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                label: const Text('Password'),
+                prefixIcon: Icon(Icons.person),
+                border: OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 8),
+            _isSignInInProcess
+                ? const CircularProgressIndicator.adaptive()
+                : ElevatedButton(
+                    style: ButtonStyle(
+                      padding: MaterialStateProperty.all(
+                        const EdgeInsets.symmetric(
+                          horizontal: 70,
+                          vertical: 10,
+                        ),
+                      ),
+                    ),
+                    onPressed: _signIn,
+                    child: const Text('Login'),
+                  ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<void> _signIn() async {
+    final login = _loginController.text;
+    final password = _passwordController.text;
+    try {
+      _isSignInInProcess = true;
+      final token =
+          await widget.authRepository.signIn(login: login, password: password);
+      _pushToChat(context, token);
+    } on AuthException catch (e) {
+      final snackBar = SnackBar(
+        content: Text(e.message),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } catch (e) {
+      log(e.toString(), error: e);
+      const snackBar = SnackBar(
+        content: Text('Error occured'),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    } finally {
+      _isSignInInProcess = false;
+    }
   }
 
   void _pushToChat(BuildContext context, TokenDto token) {
