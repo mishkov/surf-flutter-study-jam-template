68f4b9c59413b24bf0a205f842c99f250e666117
Nikita Mishkov <mishkov.dd@gmail.com>
2022-08-11 20:29:25 +0300

    Add todo

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // TODO: refacotr this piece of shit
       home: FutureBuilder<bool>(
         future: isThereTokenFuture,
         builder: (context, snapshot) {
